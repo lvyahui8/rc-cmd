31005f2c42afadaefa09154173a9457d78887cb5
lvyahui8 <lvyahui8@126.com>
2017-11-29 00:56:57 +0800

    注解方式添加及配置命令

--- src/Ruochen/Foundation/CommandTool.php
+++ src/Ruochen/Foundation/CommandTool.php
@@ -9,11 +9,19 @@
 namespace Ruochen\Foundation;
 
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Exception;
+use GetOpt\ArgumentException;
+use GetOpt\ArgumentException\Missing;
+use GetOpt\Command;
 use GetOpt\GetOpt;
+use GetOpt\Operand;
+use GetOpt\Option;
 use JBZoo\Utils\FS;
 use Monolog\Logger;
 
-class CommandTool extends GetOpt
+abstract class CommandTool extends GetOpt
 {
     /**
      * @var Logger
@@ -21,6 +29,11 @@ class CommandTool extends GetOpt
     protected $logger;
 
     protected $toolfile;
+
+    /**
+     * @var AnnotationReader
+     */
+    protected $annoReader;
 
     /**
      * CommandTool constructor.
@@ -38,6 +51,8 @@ class CommandTool extends GetOpt
     private function init()
     {
         $this->initLogger();
+        $this->initAnnoReader();
+        $this->bindOptions();
     }
 
     private function initLogger()
@@ -45,5 +60,87 @@ class CommandTool extends GetOpt
         $this->logger = new Logger((FS::filename($this->toolfile)));
     }
 
+    private function bindOptions()
+    {
 
+        $this->addOptions([
+
+            Option::create('v', 'version', GetOpt::NO_ARGUMENT)
+                ->setDescription('Show version information and quit'),
+
+            Option::create('h', 'help', GetOpt::NO_ARGUMENT)
+                ->setDescription('Show this help and quit'),
+
+        ]);
+
+
+        $ref = new \ReflectionClass(get_class($this));
+        $methods = $ref->getMethods(\ReflectionMethod::IS_PUBLIC);
+        $opreadMap = [
+            'required'  =>  Operand::REQUIRED,
+            'multiple'  =>  Operand::MULTIPLE,
+            'optional'  =>  Operand::OPTIONAL,
+        ];
+        foreach($methods as $method){
+            $commandAnno = $this->annoReader
+                ->getMethodAnnotation($method,'\Ruochen\Annotations\Command');
+
+            if($commandAnno != null){
+                $commandName = $commandAnno->name ? $commandAnno->name : $method->getName();
+                $command = Command::create($commandName,[$this,$method]);
+
+                $opreadAnno = $this->annoReader->getMethodAnnotation($method,\Ruochen\Annotations\Opread::class);
+
+                if($opreadAnno){
+                    $opread = Operand::create($opreadAnno->name
+                        ,isset($opreadMap[$opreadAnno->mode]) ? $opreadMap[$opreadAnno->mode] : Operand::REQUIRED);
+
+                    $command->addOperand($opread);
+                }
+
+                $descAnno = $this->annoReader->getMethodAnnotation($method,\Ruochen\Annotations\Desc::class);
+                if($descAnno){
+                    $command->setDescription($descAnno->value);
+                }
+                $this->addCommand($command);
+            }
+        }
+    }
+
+    private function initAnnoReader()
+    {
+        AnnotationRegistry::registerAutoloadNamespace('Ruochen\Annotations',base_path().'/src');
+        $this->annoReader = new AnnotationReader();
+    }
+
+    public function process($arguments = null)
+    {
+        $result = 0;
+        try{
+            try {
+                $result = parent::process($arguments);
+            } catch (Missing $exception) {
+                if (!$this->getOption('help')) {
+                    throw $exception;
+                }
+            }
+        } catch(ArgumentException $e){
+            file_put_contents('php://stderr', $e->getMessage() . PHP_EOL);
+            echo PHP_EOL . $this->getHelpText();
+            exit;
+        }
+
+        if ($this->getOption('version')) {
+            echo sprintf('%s: %s' . PHP_EOL, FS::filename($this->toolfile)
+                ,defined('VERSION') ? VERSION : '1.0.0' );
+            exit;
+        }
+        $command = $this->getCommand();
+        if (!$command || $this->getOption('help')) {
+            echo $this->getHelpText();
+            exit;
+        }
+
+        return  call_user_func($command->getHandler(), $this);
+    }
 }
